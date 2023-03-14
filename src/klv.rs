2a2224b3c6e9aeae1e2b9b0bb9cdef04de4c3a55
Andy Kurnia <andykurn@gmail.com>
2023-03-15 05:33:04 +0800

    update wolges to 81745293513d835269e44b2455244dccadf750db

--- src/klv.rs
+++ src/klv.rs
@@ -3,9 +3,9 @@
 use super::kwg;
 
 pub struct Klv {
-    pub kwg: kwg::Kwg,
-    pub counts: Box<[u32]>,
-    pub scaled_leaves: Box<[i16]>,
+    kwg: kwg::Kwg,
+    counts: Box<[u32]>,
+    leaves: Box<[f32]>,
 }
 
 pub static EMPTY_KLV_BYTES: &[u8] = b"\x01\x00\x00\x00\x00\x00\x40\x00\x00\x00\x00\x00";
@@ -31,23 +31,38 @@ impl Klv {
         );
         r += 4;
         let mut elts = Vec::with_capacity(lv_len as usize);
-        for _ in 0..lv_len {
-            elts.push(i16::from_le(
-                (buf[r] as u16 | (buf[r + 1] as u16) << 8) as i16,
-            ));
-            r += 2;
+        if buf.len() < 4 * lv_len as usize {
+            // klv uses i16
+            for _ in 0..lv_len {
+                elts.push(
+                    i16::from_le((buf[r] as u16 | (buf[r + 1] as u16) << 8) as i16) as f32
+                        * (1.0 / 256.0),
+                );
+                r += 2;
+            }
+        } else {
+            // klv2 uses f32
+            for _ in 0..lv_len {
+                elts.push(f32::from_bits(
+                    buf[r] as u32
+                        | (buf[r + 1] as u32) << 8
+                        | (buf[r + 2] as u32) << 16
+                        | (buf[r + 3] as u32) << 24,
+                ));
+                r += 4;
+            }
         }
         let counts = kwg.count_words_alloc();
         Klv {
             kwg,
             counts,
-            scaled_leaves: elts.into_boxed_slice(),
+            leaves: elts.into_boxed_slice(),
         }
     }
 
     #[inline(always)]
     pub fn leave(&self, leave_idx: u32) -> f32 {
-        self.scaled_leaves[leave_idx as usize] as f32 * (1.0 / 256.0)
+        self.leaves[leave_idx as usize]
     }
 
     #[inline(always)]
