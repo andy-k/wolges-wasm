dd7a633c19d9b72cba3a4779be5fa0ab0ec683f9
Andy Kurnia <andykurn@gmail.com>
2026-02-23 01:31:03 +0800

    upgrade rand

--- src/lib.rs
+++ src/lib.rs
@@ -231,8 +231,9 @@ macro_rules! use_wasm_cache {
 }
 
 thread_local! {
-    static RNG: std::cell::RefCell<Box<dyn RngCore>> =
-        std::cell::RefCell::new(Box::new(rand_chacha::ChaCha20Rng::from_os_rng()));
+    static RNG: std::cell::RefCell<Box<dyn rand::Rng>> = std::cell::RefCell::new(Box::new(
+        rand::rngs::ChaCha20Rng::try_from_rng(&mut rand::rngs::SysRng).unwrap(),
+    ));
 }
 
 #[wasm_bindgen]
